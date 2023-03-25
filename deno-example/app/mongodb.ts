d5d08fd7360460a18d50035cc80d424091babab4
Aamer Nabi <aamernabi@gmail.com>
2023-03-25 12:38:39 +0530

    update - set mongo db in env

--- deno-example/app/mongodb.ts
+++ deno-example/app/mongodb.ts
@@ -1,7 +1,13 @@
 import { MongoClient } from './deps.ts'
 
+const MONGO_URI = Deno.env.get('MONGODB_URI')
+
+if (!MONGO_URI) {
+  throw new Error('Missing MONGO_URI environment variable')
+}
+
 const client = new MongoClient()
-await client.connect('mongodb://admin:password@mongodb:27017')
+await client.connect(MONGO_URI)
 const db = client.database('denoexampledb')
 
 export default db
